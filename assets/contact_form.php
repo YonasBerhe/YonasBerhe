fecad75afa71bef193a422a551a3768295395565
YonasBerhe <yonas.berhe007@gmail.com>
2015-07-02 19:55:18 -0700

    ,

---
+++ assets/contact_form.php
@@ -0,0 +1,16 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'yonas.berhe007@gmail.com'; //<-- Put your E-Mail address here 
+    $subject = 'New Message from website';
+
+    $body = "From: $name <br> E-Mail: $email <br> Message: <br> $message";
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From:' . $email. "\r\n";
+    $headers .= 'Cc:' . $email. "\r\n";
+
+    mail ($to, $subject, $body, $headers);
+?>
